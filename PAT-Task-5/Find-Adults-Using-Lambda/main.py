07f003c1fbf7148fafff8b203a0a05143f26980e
Rooban <roobank1@gmail.com>
2026-03-10 22:16:55 +0530

    Implement adult filtering using lambda functions
    Added a script to filter and list adults from a list of people using lambda functions.

---
+++ PAT-Task-5/Find-Adults-Using-Lambda/main.py
@@ -0,0 +1,15 @@
+# Filter list of people with their names and ages using lamda function
+people = [                                                  
+    {"name": "Arjun", "age": 17},
+    {"name": "Aprita", "age": 21},
+    {"name": "Aravind", "age": 16},
+    {"name": "Arun", "age": 25},
+    {"name": "Ambru", "age": 20},
+    {"name": "Arul", "age": 15},
+    {"name": "Anu", "age": 19}                               # List of dictionaries representing people with their names and ages
+]                                                               
+
+adults = filter(lambda person: person["age"] >= 18, people)  # Filter people who are 18 or older
+names = list(map(lambda person: person["name"], adults))     # Map the remaining people to their names
+
+print("Adults:", names)
